a041b4cbc241b79794cea060a7ec7fa5705423b5
JaviM <javim@proyectoepastillero.com>
2024-11-05 19:26:05 +0100

    Subir cambios al servidor

--- Assets/Scripts/ScriptParaGitHub.cs
+++ Assets/Scripts/ScriptParaGitHub.cs
@@ -5,7 +5,7 @@ public class ScriptParaGitHub : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Codigo escrito por Javi
     }
 
     // Update is called once per frame
