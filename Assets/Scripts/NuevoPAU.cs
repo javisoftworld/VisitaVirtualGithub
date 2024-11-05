770638b646a4567df4aa5a5ad531d28b546bee8b
pausama3 <pausama_93@hotmail.com>
2024-11-05 19:24:31 +0100

    modificacionespau

--- Assets/Scripts/NuevoPAU.cs
+++ Assets/Scripts/NuevoPAU.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
 
-public class ScriptParaGitHub : MonoBehaviour
+public class NuevoPAU : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //COMENTADO PAU
     void Start()
     {
         
