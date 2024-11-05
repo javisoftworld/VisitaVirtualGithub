bc3275558cb287cb279c6a07712e52a71d2c2fa4
JaviM <javim@proyectoepastillero.com>
2024-11-05 19:05:41 +0100

    Segunda subida

--- Assets/Scripts/EventosClase.cs
+++ Assets/Scripts/EventosClase.cs
@@ -16,7 +16,7 @@ public class EventosClase : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Hola caracola");
     }
     public void CrearManta()
     {
